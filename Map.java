3b8371746b30e7a1844bb1c282c935606aa3b5ee
Deniel Saskin <96053994+DeenkaS@users.noreply.github.com>
2022-10-27 11:12:40 +0200

    ok

--- Map.java
+++ Map.java
@@ -2,26 +2,27 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Map {
-    private City[] cities;
+    public City[] cities;
     private final int mod = 541;
 
     public Map(String file) {
         cities = new City[mod];
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
-            int i = 0;
-            int j = 0;
 
             while ((line = br.readLine()) != null) {
                 String[] row = line.split(",");
+
                 City city1 = lookup(row[0]);
                 City city2 = lookup(row[1]);
+
                 Integer distance = Integer.parseInt(row[2]);
 
                 city1.addConnection(city2, distance);
                 city2.addConnection(city1, distance);
 
-
+                hashadd(city1);
+                hashadd(city2);
 
             }
         } catch (Exception e) {
@@ -38,7 +39,7 @@ public class Map {
     }
 
     // takes cityname. returns city if found in list. otherwise creates new city and
-    // adds to list.
+    // returns that.
     private City lookup(String cityName) {
         int i = hash(cityName);
         if (cities[i] != null && cities[i].cityName.equals(cityName)) {
@@ -55,6 +56,20 @@ public class Map {
             cities[hash(cityName)] = newCity;
             return newCity;
 
+        }
+    }
+
+    private void hashadd(City input) {
+        int i = hash(input.cityName);
+        if (cities[i] == null) {
+            cities[i] = input;
+        } else if (cities[i] == input) {
+            return;
+        } else {
+            while (cities[i] != null) {
+                i++;
+            }
+            cities[i] = input;
         }
     }
 }
